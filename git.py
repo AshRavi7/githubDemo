bebb932d44986c634ed499ace0401d16a5cdf38f
AshRavi7 <ashravi7@gmail.com>
2020-09-14 19:03:06 +0530

    second commit

--- git.py
+++ git.py
@@ -1,4 +1,4 @@
-def hello(arg1="Good evening!",arg2="Thank you for enrolling into the free Intro to python course",arg3="Please get in touch with the Byte academy marketting team for furthur engagemnets"):
-    return arg1+" "+arg2+" "+arg3
+def hello(arg1="Good evening!",arg2="Thank you for enrolling into the free Intro to python course",arg3="Please get in touch with the Byte academy marketting team for furthur engagements"):
+    return arg1+" "+arg2+" "+arg3+"."
 
 print(hello())
